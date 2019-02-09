7683b38446b555f4bbb4c754ddf23ba1b5bfe129
Saurabh Khattry <khattry90@gmail.com>
2019-02-09 18:49:59 +0530

    Create rotatelist.py

---
+++ NPTEL/rotatelist.py
@@ -0,0 +1,24 @@
+'''
+A list rotation consists of taking the last element and moving it to the front. For instance, if we rotate the list [1,2,3,4,5], we get [5,1,2,3,4]. If we rotate it again, we get [4,5,1,2,3].
+Write a Python function rotatelist(l,k) that takes a list l and a positive integer k and returns the list l after k rotations. If k is not positive, your function should return l unchanged. Note that your function should not change l itself, and should return the rotated list.
+Here are some examples to show how your function should work.
+>>> rotatelist([1,2,3,4,5],1)
+[5, 1, 2, 3, 4]
+>>> rotatelist([1,2,3,4,5],3)
+[3, 4, 5, 1, 2]
+>>> rotatelist([1,2,3,4,5],12)
+[4, 5, 1, 2, 3]
+'''
+
+def rotatelist(l, k):
+    a = l
+    i = 0
+
+    while i < k:
+        b = []
+        b.append(a[-1])
+        a = b + a[:-1]
+        i += 1
+    return a
+
+print(rotatelist([1,2,3,4,5],2))
